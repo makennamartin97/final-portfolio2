d01bc93b439af231548996599b81c57ef8df6440
makennamartin97 <makennamartin97@yahoo.com>
2020-09-02 18:33:13 -0400

    Updates

---
+++ precache-manifest.15115f0b76573a2bc484ad59289b3a2b.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5722f71f089194627aa268ea487bca32",
+    "url": "/final-portfolio2/index.html"
+  },
+  {
+    "revision": "51aff1bfdbd199ab6bf7",
+    "url": "/final-portfolio2/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "148e9088bacb70035b43",
+    "url": "/final-portfolio2/static/css/main.aae4d756.chunk.css"
+  },
+  {
+    "revision": "51aff1bfdbd199ab6bf7",
+    "url": "/final-portfolio2/static/js/2.3201dd94.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/final-portfolio2/static/js/2.3201dd94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "148e9088bacb70035b43",
+    "url": "/final-portfolio2/static/js/main.f7fc6f06.chunk.js"
+  },
+  {
+    "revision": "f9e9577d9b545d6a75f2",
+    "url": "/final-portfolio2/static/js/runtime-main.74c4a1bc.js"
+  },
+  {
+    "revision": "c9c15575c71d8b0e7a4bfec57033c7ed",
+    "url": "/final-portfolio2/static/media/add.c9c15575.png"
+  },
+  {
+    "revision": "7d825e2ce019746c592f6e9843f47385",
+    "url": "/final-portfolio2/static/media/all2.7d825e2c.png"
+  },
+  {
+    "revision": "76ee2795af0db2664d30cca87b67d947",
+    "url": "/final-portfolio2/static/media/aug18again-1.76ee2795.png"
+  },
+  {
+    "revision": "1e333e3cf41b386b911879efb2f1f5c1",
+    "url": "/final-portfolio2/static/media/aug18again-2.1e333e3c.png"
+  },
+  {
+    "revision": "e07a4444c04c7e851c4fb0f8e783587d",
+    "url": "/final-portfolio2/static/media/crudadd.e07a4444.png"
+  },
+  {
+    "revision": "b183e05cd006a1da9190dd78b2ad6ed0",
+    "url": "/final-portfolio2/static/media/crudall.b183e05c.png"
+  },
+  {
+    "revision": "220241dc47da4e685c137f8504b18ca6",
+    "url": "/final-portfolio2/static/media/cruddetail.220241dc.png"
+  },
+  {
+    "revision": "d086c3e48845c6094669c6c3c3f2be21",
+    "url": "/final-portfolio2/static/media/crudmain.d086c3e4.png"
+  },
+  {
+    "revision": "efb3156addea0608d9d5096969f202d6",
+    "url": "/final-portfolio2/static/media/crudmain2.efb3156a.png"
+  },
+  {
+    "revision": "c538aa73278e9df9ca61f5b90159f850",
+    "url": "/final-portfolio2/static/media/finaldetails.c538aa73.png"
+  },
+  {
+    "revision": "62462bc7b0dac8c2c4f039de48590acd",
+    "url": "/final-portfolio2/static/media/login.62462bc7.png"
+  },
+  {
+    "revision": "452c194e0317fc72d06b1734e0e71845",
+    "url": "/final-portfolio2/static/media/main.452c194e.png"
+  },
+  {
+    "revision": "d1e47e7c8feb55862dbd0bf909b281fd",
+    "url": "/final-portfolio2/static/media/pet1.d1e47e7c.png"
+  },
+  {
+    "revision": "8e125083b10a81ffbbab7b84f7575a4a",
+    "url": "/final-portfolio2/static/media/pet2.8e125083.png"
+  },
+  {
+    "revision": "fc7d7c33796d63cbedf6d86a0bca0f6c",
+    "url": "/final-portfolio2/static/media/pet3.fc7d7c33.png"
+  },
+  {
+    "revision": "eaa691a8c6b77feb20d3362468d32c5e",
+    "url": "/final-portfolio2/static/media/pet4.eaa691a8.png"
+  },
+  {
+    "revision": "d59c0d49dc730b75d8edba7220d4fae2",
+    "url": "/final-portfolio2/static/media/q.d59c0d49.png"
+  },
+  {
+    "revision": "caaa5611695e7e7fade94b0c9c974c4d",
+    "url": "/final-portfolio2/static/media/q2.caaa5611.png"
+  }
+]);
